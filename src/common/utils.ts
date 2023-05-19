cef3fdd7a513bfe6dfb3dadcb9db81077aa72840
Madan <m4adan@gmail.com>
2023-05-19 16:30:21 +0530

    *Added kafkajs and mongoose 	*Implemented Kafka consumer for updating bookings 	*Included kakfa deployment files

---
+++ src/common/utils.ts
@@ -0,0 +1,3 @@
+export const getNewId = (prefix: string) => {
+	return `${prefix}_${Math.floor(1000 + Math.random() * 900000).toString()}`;
+};
---
+++ src/models/booking.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import { Schema, Document } from 'mongoose';
+import { bookingStatusEnum } from '../common/enums';
+
+const bookingSchema = new Schema({
+	bookingId: String,
+	origin: String,
+	destination: String,
+	fare: Number,
+	riderId: String,
+	partnerId: String,
+	vehicleId: String,
+	status: { type: String, enum: bookingStatusEnum },
+	createdAt: {
+		type: Date,
+		default: () => {
+			return Date.now();
+		},
+	},
+	completedAt: Date,
+	scheduledCompletionTime: {
+		type: Date,
+		default: function () {
+			return Date.now() + 54 * 60 * 1000;
+		},
+	},
+});
+
+export default mongoose.model('Booking', bookingSchema);
